d7199a7261107e0434e88853fa3af6eeb9612004
darianjade <darianperez1616@gmail.com>
2021-04-26 13:18:46 -0500

    Added classes for lists

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -13,6 +13,7 @@ namespace Engine
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
 
         public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) 
             : base(currentHitPoints, maximumHitPoints)
@@ -22,6 +23,7 @@ namespace Engine
             MaximumDamage = maximumDamage;
             RewardExperiencePoints = RewardExperiencePoints;
             RewardGold = RewardGold;
+            LootTable = new List<LootItem>();
         }
     }
 }
