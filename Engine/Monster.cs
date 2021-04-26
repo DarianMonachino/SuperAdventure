31963c00b851f5941b6f6d73098c06ce7f0b32af
darianjade <darianperez1616@gmail.com>
2021-04-26 11:51:47 -0500

    Clean up to class objects

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -13,5 +13,15 @@ namespace Engine
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+
+        public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) 
+            : base(currentHitPoints, maximumHitPoints)
+        {
+            Id = id;
+            Name = name;
+            MaximumDamage = maximumDamage;
+            RewardExperiencePoints = RewardExperiencePoints;
+            RewardGold = RewardGold;
+        }
     }
 }
